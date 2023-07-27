79a3cda953e4ec1d8e93718223ed29444d098e0d
virajpatil007 <virajpatil2512@gmail.com>
2023-07-27 14:04:53 +0530

    first commit

---
+++ xavier_backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function login(Request $req){
+        $user=User::Where('email',$req->email)->first();
+        if($user && $req->password===$user->password){
+        return $user;
+        }else{
+            return response([
+                "data"=>[ 'password dont Match']
+        ]    ) ;
+        }
+    }
+     
+}
